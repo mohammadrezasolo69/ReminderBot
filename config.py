b5a893c7360ff0ed7bdc245f0b6b40a61dc7352b
Mohammadreza <mohammdrezasolo69@gmail.com>
2024-04-03 13:07:41 +0330

    config logger

--- config.py
+++ config.py
@@ -1,7 +1,14 @@
+import logging
+
 from dynaconf import Dynaconf
 
+# ------------------------------ config dynaconf ---------------------------------------
 settings = Dynaconf(
     envvar_prefix="DYNACONF",
     settings_files=['config.yaml'],
 )
 
+# ------------------------------ config logger ---------------------------------------
+logging.basicConfig(format="%(asctime)s - %(name)s - %(levelname)s - %(message)s", level=logging.INFO)
+logging.getLogger("httpx").setLevel(logging.WARNING)
+logger = logging.getLogger(__name__)
